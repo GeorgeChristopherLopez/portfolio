79961b793779daa0332c6dcb9c403a4f59f50233
GeorgeChristopherLopez <georgechristopherlopez@gmail.com>
2020-10-18 16:32:25 -0400

    Updates

---
+++ precache-manifest.d76e292289652d78897a1c5ea1f49f6d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ba149f305cb1eb96219624555b6db9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5596aea77fe1ba74967a",
+    "url": "/portfolio/static/css/main.bf4c1e3d.chunk.css"
+  },
+  {
+    "revision": "ab49f60fdc53362a2949",
+    "url": "/portfolio/static/js/2.3c50fc71.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.3c50fc71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5596aea77fe1ba74967a",
+    "url": "/portfolio/static/js/main.607373a9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "13f045ed4cda81fc5804169263f52fb1",
+    "url": "/portfolio/static/media/elite_driving_thumbnail.13f045ed.png"
+  },
+  {
+    "revision": "c878cef889264ea1d8f3f82accc27c6f",
+    "url": "/portfolio/static/media/flixcentral_thumbnail.c878cef8.png"
+  },
+  {
+    "revision": "19ffb80ae618202766483f04e96cbba7",
+    "url": "/portfolio/static/media/google_books_search.19ffb80a.png"
+  }
+]);
